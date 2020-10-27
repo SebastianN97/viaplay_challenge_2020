c3f7e0539708f32f932dd44ddec055ac85d074ae
SebastianN97 <sebbe132132@gmail.com>
2020-10-27 18:52:12 +0100

    Creating first acceptance test.

---
+++ cypress/integration/VisitorCanSeeTheMostPopularSeries.feature.js
@@ -0,0 +1,16 @@
+describe("Visitor can see", () => {
+  beforeEach(() => {
+    cy.server()
+    cy.route({
+      method: "GET",
+      url: "https://cors-anywhere.herokuapp.com/https://content.viaplay.se/pc-se/serier/samtliga",
+      response: "fixtures:series.json"
+    })
+    cy.visit('http://localhost:3000')
+  })
+
+  it('list of popular series', () => {
+    cy.get("[data-cy='header']").should('be.visible')
+    cy.get('.display-show').to.have.lengthOf(10)
+  })
+})
